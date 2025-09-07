9a1c2569e262f55adb5d7f77ad7cafb52b872631
Endermandels <elijahdelavar@gmail.com>
2025-09-07 08:14:44 -0700

    Add a lot of Cats and tweak soft collision

--- components/soft_collision.gd
+++ components/soft_collision.gd
@@ -2,11 +2,13 @@ extends Area2D
 class_name SoftCollision
 
 @export_group("Settings")
-@export var push_strength: float = 25.0 ## How fast to push away
+@export var push_strength: float = 75.0 ## How fast to push away
+@export var push_distance: float = 100.0 ## How far to push away
 
 func handle_collision(body: CharacterBody2D) -> void:
     var areas = get_overlapping_areas()
     var push_vector = Vector2.ZERO
-    if areas.size() > 0:
-        push_vector = areas[0].global_position.direction_to(global_position).normalized()
-    body.velocity += body.velocity.move_toward(push_vector * push_strength, push_strength)
+    for area in areas:
+        push_vector += area.global_position.direction_to(global_position)
+    push_vector = push_vector.normalized()
+    body.velocity += body.velocity.move_toward(push_vector * push_distance, push_strength)
