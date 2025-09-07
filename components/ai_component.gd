b3b0ea3857417315aeedc7f47ce0685beaf4fa35
Endermandels <elijahdelavar@gmail.com>
2025-09-07 07:20:20 -0700

    Stop cat movement once in range of player, Expand player detection once tamed

--- components/ai_component.gd
+++ components/ai_component.gd
@@ -1,6 +1,10 @@
 extends Node
 class_name AIComponent
 
+@export_group("Settings")
+@export var close_enough_range: float = 60.0 ## Distance from player to stop moving
+@export var follow_range: float = 350.0 ## Radius of player detection once tamed
+
 @export_group("External Nodes")
 @export var target_detector: TargetDetector
 @export var exclamation_mark: ExclamationMark
@@ -11,7 +15,7 @@ var input_vector: Vector2 = Vector2.ZERO ## Normalized input vector for moving
 func handle_artificial_input(body: Node2D) -> void:
     # Only move towards player once tamed
     # TODO: Make movement outside of being tamed as well, like wandering
-    if target_detector.target and tamed:
+    if tamed and target_detector.target and body.global_position.distance_to(target_detector.target.global_position) > close_enough_range:
         input_vector = body.global_position.direction_to(target_detector.target.global_position)
     else:
         print(input_vector)
@@ -21,3 +25,4 @@ func _process(_delta: float) -> void:
     if not tamed and target_detector.target and target_detector.target.input_component.interact_pressed:
         tamed = true
         exclamation_mark.disable()
+        target_detector.set_detection_radius(follow_range)
--- components/target_detector.gd
+++ components/target_detector.gd
@@ -3,14 +3,20 @@ class_name TargetDetector
 
 ''' Only handles a single target '''
 
+@export_group("Internal Nodes")
+@export var collision_shape: CollisionShape2D
+
 var target: Node2D = null
 
 func _ready() -> void:
-    body_entered.connect(_on_body_entered)
-    body_exited.connect(_on_body_exited)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func set_detection_radius(radius: float) -> void:
+	collision_shape.shape.radius = radius
 
 func _on_body_entered(body: Node2D) -> void:
-    target = body
+	target = body
 
 func _on_body_exited(_body: Node2D) -> void:
-    target = null
+	target = null
