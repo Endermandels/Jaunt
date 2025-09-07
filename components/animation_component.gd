489721391844b1c6815d99d0c9f4e555396f4ca7
Endermandels <elijahdelavar@gmail.com>
2025-09-07 08:32:10 -0700

    Add shadows and offset player sprite

--- components/animation_component.gd
+++ components/animation_component.gd
@@ -8,12 +8,17 @@ class_name AnimationComponent
 @export var sprite: Sprite2D
 @export var anim_player: AnimationPlayer
 
-func handle_facing_horizontal_direction(direction: float) -> void:
-    """ Direction 1 = Right, Direction -1 = Left, Direction 0 = not moving horizontally """
+func handle_facing_horizontal_direction(direction: float, offset_x: float = 0.0) -> void:
+    """ 
+    direction 1 = Right, direction -1 = Left, direction 0 = not moving horizontally
+    offset_x: how much to move the sprite to the right when facing right or left when facing left
+    """
     if direction < 0:
         sprite.flip_h = true
+        sprite.offset.x = -offset_x
     elif direction > 0:
         sprite.flip_h = false
+        sprite.offset.x = offset_x
 
 func handle_movement_animation(body: CharacterBody2D) -> void:
     if body.velocity.length() > idle_tolerance:
--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,6 @@ class_name Player
 
 func _physics_process(_delta: float) -> void:
     movement_component.handle_movement(self, input_component.input_vector)
-    animation_component.handle_facing_horizontal_direction(input_component.input_vector.x)
+    animation_component.handle_facing_horizontal_direction(input_component.input_vector.x, 3)
     animation_component.handle_movement_animation(self)
     move_and_slide()
