a4ec7a310f3cbb4fbf2a353a4651cd5ab1494867
Endermandels <elijahdelavar@gmail.com>
2025-09-14 09:42:03 -0700

    Spawn in groups with a normal distribution and add despawn

--- scripts/animal_spawner.gd
+++ scripts/animal_spawner.gd
@@ -11,7 +11,8 @@ const FerretScene = preload("res://scenes/animals/ferret.tscn")
 const FoxScene = preload("res://scenes/animals/fox.tscn")
 
 @export_group("Settings")
-@export var spawn_chance: float = 0.1 ## Percentage chance of spawning an animal on successful spawn check
+@export_range(0, 1) var spawn_chance: float = 0.1 ## Percentage chance of spawning an animal on successful spawn check
+@export_range(0, 1000) var max_num_spawned: int = 5 ## Maximum number of spawned animals on any spawn interval
 
 @export_group("External Nodes")
 @export var player: Player
@@ -19,40 +20,45 @@ const FoxScene = preload("res://scenes/animals/fox.tscn")
 
 @export_group("Internal Nodes")
 @export var spawn_area: CollisionShape2D
-@export var timer: Timer
+@export var spawn_interval: Timer
 
 var min_dist: float = 0.0
 
-static func get_random_animal() -> Animal:
-	'''
-	Returns a random Animal instance.
-	'''
-	var rnd = randi_range(0, 2)
-	if rnd == 1:
-		return FerretScene.instantiate()
-	if rnd == 2:
-		return FoxScene.instantiate()
-	return CatScene.instantiate()
+static func get_random_animal() -> PackedScene:
+    '''
+    Returns a random Animal instance.
+    '''
+    var rnd = randi_range(0, 2)
+    if rnd == 1:
+        return FerretScene
+    if rnd == 2:
+        return FoxScene
+    return CatScene
 
 func _ready() -> void:
-	var vrect = get_viewport_rect()
-	min_dist = vrect.get_center().distance_to(vrect.position) + 16 # Extra padding
-	
-	timer.timeout.connect(check_for_spawn)
+    var vrect = get_viewport_rect()
+    min_dist = vrect.get_center().distance_to(vrect.position) + 16 * max_num_spawned # Extra padding
+    
+    spawn_interval.timeout.connect(check_for_spawn)
 
 func spawn_animal() -> void:
-	var dist = randf_range(min_dist, spawn_area.shape.radius)
-	var angle = randf_range(0, 2*PI)
-	var dir = Vector2(cos(angle), sin(angle))
-	var pos = global_position + dir * dist
-	var animal = get_random_animal()
-	animal.global_position = pos
-	spawn_node.add_child(animal)
+    var dist = randf_range(min_dist, spawn_area.shape.radius)
+    var angle = randf_range(0, 2*PI)
+    var dir = Vector2(cos(angle), sin(angle))
+    var pos = global_position + dir * dist
+    var animal_scene = get_random_animal()
+
+    var rnd_num = clampi(randfn(0.0, 2.0), 0, max_num_spawned)
+
+    for i in range(0, rnd_num + 1):
+        var animal = animal_scene.instantiate()
+        animal.global_position = pos + Vector2(randf_range(-i * 4, i * 4), randf_range(-i * 4, i * 4))
+        spawn_node.add_child(animal)
 
 func check_for_spawn() -> void:
-	# Detect player movement
-	if global_position != player.global_position:
-		global_position = player.global_position
-		# Check for spawning an animal
-		if randf() < spawn_chance:
-			spawn_animal()
+    # Detect player movement
+    if global_position != player.global_position:
+        global_position = player.global_position
+        # Check for spawning an animal
+        if randf() < spawn_chance:
+            spawn_animal()
--- scripts/animals/animal.gd
+++ scripts/animals/animal.gd
@@ -7,6 +7,18 @@ class_name Animal
 @export var animation_component: AnimationComponent
 @export var soft_collision: SoftCollision
 
+func _ready() -> void:
+    get_tree().create_timer(30).timeout.connect(check_despawn) # Despawn after a minute if not tamed
+
+func check_despawn() -> void:
+    if not ai_component.tamed:
+        # Only despawn if not currently on the screen
+        var viewport_size = get_viewport().get_visible_rect().size
+        var camera_rect = Rect2(get_viewport().get_camera_2d().get_screen_center_position() - viewport_size * 0.5, viewport_size)
+        if not camera_rect.has_point(global_position):
+            queue_free()
+        else:
+            get_tree().create_timer(30).timeout.connect(check_despawn) # Despawn after a minute if not tamed
 
 func _physics_process(_delta: float) -> void:
     ai_component.handle_artificial_input(self)
